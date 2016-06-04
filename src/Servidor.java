2237c8710569f0641c6cc85c7725d0aa012b9499
florrv <florenciarv@gmail.com>
2016-06-04 12:29:07 -0700

    agregue clase Servidor

---
+++ src/Servidor.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Servidor {
+	// Constructor
+	private ArrayList<POI> colPOIs = new ArrayList<POI>();
+	private ArrayList<Administrador> colAdmins = new ArrayList<Administrador>();
+
+	// Metodos
+	public boolean ObtenerEstadisticas(){
+		return true;
+	}
+	
+	public boolean Loguin(Administrador[] colAdmins, String adminBuscado){
+		for (Administrador s: colAdmins)
+		{
+			if(s.equals(adminBuscado))
+				return true;
+		}
+		return false;
+
+		}
+	}
+	
+}
+
