4cfd41e575ce4e7d72a5e55070767a1b6419f5ff
florrv <florenciarv@gmail.com>
2016-06-04 12:41:52 -0700

    modificacion DispositivoConsulta

--- src/DispositivoConsulta.java
+++ src/DispositivoConsulta.java
@@ -1,11 +1,7 @@
-import java.util.ArrayList;
-
 public class DispositivoConsulta {
 
 	private double longitud;
 	private double latitud;
 	private String zona;
-	private ArrayList<POI> POIs = new ArrayList<POI>();
-	//private ArrayList<Adminstrador> Admins = new ArrayList<Administrador>();
 	//nuevo
 }
