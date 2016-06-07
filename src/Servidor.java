826f3a88030e8a5afbbc654b2cd9d02b23d65b97
florrv <florenciarv@gmail.com>
2016-06-06 20:32:33 -0700

    modif en Servidor

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,10 +1,40 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Servidor {
 	// Constructor
 	private ArrayList<POI> colPOIs = new ArrayList<POI>();
 	private ArrayList<Administrador> colAdmins = new ArrayList<Administrador>();
 
+	// ***************************************************************************
+	// Setters
+	// ***************************************************************************
+		public void iniccolPOIs() {
+			List<POI> colPOIs = new ArrayList<POI>();
+		}
+	
+		public void cargarPOI(POI unPOI) {
+			this.colPOIs.add(unPOI);
+		}
+		public void iniccolAdmins() {
+			List<Administrador> colAdmins = new ArrayList<Administrador>();
+		}
+	
+		public void addAdmin(Administrador unAdmin) {
+			this.colAdmins.add(unAdmin);
+		}
+		
+	// ***************************************************************************
+	// Getters
+	// ***************************************************************************
+		public ArrayList<POI> getcolPOIs() {
+			return colPOIs;
+		}
+		
+		public ArrayList<Administrador> getcolAdmins() {
+			return colAdmins;
+		}
+	
 	// Metodos
 	public boolean ObtenerEstadisticas(){
 		return true;
@@ -19,7 +49,17 @@ public class Servidor {
 		return false;
 
 		}
-	}
 	
-}
-
+	public void eliminarPOI(POI unPOI){
+		if(colPOIs.contains(unPOI) == true){
+		colPOIs.remove(unPOI);
+		}
+	}	
+	
+	public void eliminarAdmin(Administrador unAdmin){
+		if(colAdmins.contains(unAdmin) == true){
+		colAdmins.remove(unAdmin);
+		}
+	}	
+	
+	}
