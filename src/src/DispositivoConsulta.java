fa93ee5b534e9a07c7b233a30cc173c41e0e27e3
rperal22 <osboxes@osboxes.(none)>
2016-06-17 23:34:36 -0300

    subo DispositivoConsulta

--- src/src/DispositivoConsulta.java
+++ src/src/DispositivoConsulta.java
@@ -4,5 +4,50 @@ public class DispositivoConsulta {
 	private double longitud;
 	private double latitud;
 	private String zona;
-	//nuevo
+	//nuevo rodo
+
+	
+	
+	
+	// *****no deberia tener un nombre?
+
+
+	
+		
+	// ***************************************************************************
+// Setters
+// ***************************************************************************
+		
+		public void setLatitud(Double latitud) {
+			this.latitud = latitud;
+		}
+		
+		public void setLongitud(Double longitud){
+			this.longitud= longitud;
+		}
+		
+		public void setZona(String zona){
+			this.zona= zona;
+		}
+
+
+
+		// ***************************************************************************
+		// Getters
+		// ***************************************************************************
+		
+		public double getLatitud(){
+			return latitud;
+		}
+		
+		public double getLongitud(){
+			return longitud;
+		}
+		
+		public String getZona(){
+			return zona;
+		}
 }
+		// ***************************************************************************
+		// Methods
+		// ***************************************************************************
