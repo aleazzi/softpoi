a054e3f6d6e196455584d1a38845f09788ece8aa
florrv <florenciarv@gmail.com>
2016-06-20 16:27:07 -0700

    Comuna con constructor

--- src/src/Comuna.java
+++ src/src/Comuna.java
@@ -10,44 +10,10 @@ public class Comuna {
 	private double limEste;
 	private double limOeste;
 	
-	// ***************************************************************************
-	// Setters
-	// ***************************************************************************
-	
-	public void setlimSur(double unLimSur){
+	public Comuna(double unLimSur, double unLimNorte, double unLimEste, double unLimOeste) {
 		this.limSur = unLimSur;
-	}
-	
-	public void setlimNorte(double unLimNorte){
 		this.limNorte = unLimNorte;
-	}
-
-	public void setlimEste(double unLimEste){
 		this.limEste = unLimEste;
-	}
-	
-	public void setlimOeste(double unLimOeste){
 		this.limOeste = unLimOeste;
 	}
-	
-	// ***************************************************************************
-	// Getters
-	// ***************************************************************************
-	
-	public double getlimSur(){
-		return limSur;
-	}
-	
-	public double getlimNorte(){
-		return limNorte;
-	}
-	
-	public double getlimEste(){
-		return limEste;
-	}
-	
-	public double getlimOeste(){
-		return limOeste;
-	}
-	
 }
